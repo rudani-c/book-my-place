acb85d72bab9b33281f628c8427623696733807e
CHANDRAKANT RUDANI <rudani.c@outlook.com>
2020-09-10 08:22:16 -0400

    places page layout and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthPageModule) }
+  { path: '', redirectTo: 'places', pathMatch: 'full' },
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthPageModule) },
+  { path: 'places', loadChildren: () => import('./places/places.module').then(m => m.PlacesPageModule) }
 ];
 
 @NgModule({
