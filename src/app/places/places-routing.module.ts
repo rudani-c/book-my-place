56e796315546333f575bb084a1a29908eebc56c0
CHANDRAKANT RUDANI <rudani.c@outlook.com>
2020-09-10 08:35:31 -0400

    discover tab layout and routing

--- src/app/places/places-routing.module.ts
+++ src/app/places/places-routing.module.ts
@@ -4,7 +4,35 @@ import { RouterModule, Routes } from '@angular/router';
 import { PlacesPage } from './places.page';
 
 const routes: Routes = [
-  
+  {
+    path: 'tabs',
+    component: PlacesPage,
+    children: [
+      {
+        path: 'discover',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('./discover/discover.module').then(m => m.DiscoverPageModule)
+          },
+          {
+            path: ':placeId',
+            loadChildren: () => import('./discover/place-detail/place-detail.module').then(m => m.PlaceDetailPageModule)
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: '/places/tabs/discover',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/places/tabs/discover',
+    pathMatch: 'full'
+  }
 ];
 
 @NgModule({
