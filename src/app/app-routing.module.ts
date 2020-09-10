d83ef2245aafdcdbe7430389c0996a606c0ba042
CHANDRAKANT RUDANI <rudani.c@outlook.com>
2020-09-10 08:50:13 -0400

    bookings page layout and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'places', pathMatch: 'full' },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthPageModule) },
-  { path: 'places', loadChildren: () => import('./places/places.module').then(m => m.PlacesPageModule) }
+  { path: 'places', loadChildren: () => import('./places/places.module').then(m => m.PlacesPageModule) },
+  { path: 'bookings', loadChildren: () => import('./bookings/bookings.module').then(m => m.BookingsPageModule) }
 ];
 
 @NgModule({
