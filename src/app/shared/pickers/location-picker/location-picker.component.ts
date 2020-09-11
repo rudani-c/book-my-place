60f864bf612a6b1032839cb4e7643f5b866a91a8
CHANDRAKANT RUDANI <rudani.c@outlook.com>
2020-09-11 12:15:50 -0400

    location picker

--- src/app/shared/pickers/location-picker/location-picker.component.ts
+++ src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 import { MapModalComponent } from '../../map-modal/map-modal.component';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-location-picker',
@@ -9,16 +12,40 @@ import { MapModalComponent } from '../../map-modal/map-modal.component';
   styleUrls: ['./location-picker.component.scss']
 })
 export class LocationPickerComponent implements OnInit {
-  constructor(private modalCtrl: ModalController) {}
+  constructor(private modalCtrl: ModalController, private http: HttpClient) {}
 
   ngOnInit() {}
 
   onPickLocation() {
     this.modalCtrl.create({ component: MapModalComponent }).then(modalEl => {
       modalEl.onDidDismiss().then(modalData => {
-        console.log(modalData.data);
+        if (!modalData.data) {
+          return;
+        }
+        this.getAddress(modalData.data.lat, modalData.data.lng).subscribe(
+          address => {
+            console.log(address);
+          }
+        );
       });
       modalEl.present();
     });
+  }
+
+  private getAddress(lat: number, lng: number) {
+    return this.http
+      .get<any>(
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${
+          environment.googleMapsAPIKey
+        }`
+      )
+      .pipe(
+        map(geoData => {
+          if (!geoData || !geoData.results || geoData.results.length === 0) {
+            return null;
+          }
+          return geoData.results[0].formatted_address;
+        })
+      );
   }
 }
