4117db6caab6d75c9990bc89e4bf5cb3ae7b343d
CHANDRAKANT RUDANI <rudani.c@outlook.com>
2020-09-11 00:47:09 -0400

    Added segmented button to filter places list

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
+import { SegmentChangeEventDetail } from '@ionic/core';
 
 import { PlacesService } from '../places.service';
 import { Place } from '../place.model';
@@ -25,5 +26,9 @@ export class DiscoverPage implements OnInit {
 
   onOpenMenu() {
     this.menuCtrl.toggle();
+  }
+
+  onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>) {
+    console.log(event.detail);
   }
 }
